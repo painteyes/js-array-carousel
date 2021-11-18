05e3a7bee8a3a821e6814dc0e491e10cfe277d0b
painteyes <andreantoniocchipinti@gmail.com>
2021-11-18 16:34:57 +0100

    commit changes

---
+++ Milestone-2/js/script.js
@@ -0,0 +1,62 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+const imagesContainer = document.querySelector('.images-container');
+const thumbnails = document.querySelector('.thumbnails');
+
+for (let i = 0; i < items.length; i++){
+    
+    const thisPath = items[i];
+    const thisTitle = title[i];
+    const thisText = text[i];
+
+    // new image template
+    const singleImageDiv = `
+    <div class="single-image active">
+
+        <img src="${thisPath}" alt=""> 
+
+        <div class="single-image-text">
+            <h3>${thisTitle}</h3>
+            <p>${thisText}</p>
+        </div>
+    </div>
+    `;
+
+    // new thumb template
+    const singleThumbDiv = `
+    <div class="single-thumb">
+        <img src="${thisPath}" alt="">
+    </div>  
+    `;
+
+    // append new image and new thumb
+    imagesContainer.innerHTML += singleImageDiv;
+    thumbnails.innerHTML += singleThumbDiv;
+}
+
+
+
+
+
